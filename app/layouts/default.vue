552be9e71ce73024ca07f22374b92f0fc51288ee
DanielCC <139388427+DanielCC-dev@users.noreply.github.com>
2026-04-19 19:55:49 -0500

    adicion de whatsapp

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-import Footer from '~/components/footer.vue';
-import Header from '~/components/header.vue';
-
 
 </script>
 
@@ -12,13 +9,14 @@ import Header from '~/components/header.vue';
             <slot />
         </main>
         <Footer />
+        <UiWhatsAppFloat phone="51987151798" message="Hola, vengo desde tu web" :delay="5000" />
     </div>
 </template>
 
 <style scoped>
 main {
     padding-top: 55px;
-    font-family: 'DM Sans', sans-serif;
+    font-family: var(--font-body);
     background: var(--color-bg-light);
     color: var(--color-text-on-light);
     overflow-x: hidden;
